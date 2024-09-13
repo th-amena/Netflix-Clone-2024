594b29a6b4415532f86dbcaf5035018d364f6117
th-amena <hewan.tom@gmail.com>
2024-09-13 06:49:17 -0400

    config-deploy

--- netflix-clone/src/components/Banner/Banner.jsx
+++ netflix-clone/src/components/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from '../../utils/axios'
+//axios calls baseURL 
 import requests from '../../utils/requests'
 import './Banner.css'
 
